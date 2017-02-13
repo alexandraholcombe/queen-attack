5f029520e37051dd6999222549261b30c934b6c9
Alexandra Holcombe and Caitlin Hines <alexandra+caitlin@>
2017-02-13 14:21:26 -0800

    Add Queen constructor to queen class

---
+++ Objects/Queen.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System;
+using Nancy;
+
+namespace QueenAttack.Objects
+{
+  public class Queen
+  {
+    private int _xCoord;
+    private int _yCoord;
+
+    public Queen(int xCoord, int yCoord)
+    {
+      _xCoord = xCoord;
+      _yCoord = yCoord;
+    }
+
+    public string CanAttack()
+    {
+      if ()
+    }
+  }
+}
