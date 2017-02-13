bc62056b85c8f662ebf3a71b00d872363e640564
Alexandra Holcombe and Caitlin Hines <alexandra+caitlin@>
2017-02-13 14:38:57 -0800

    Add second successful test to return false if coordinates do not align

--- Tests/QueenTest.cs
+++ Tests/QueenTest.cs
@@ -9,5 +9,12 @@ namespace QueenAttack.Objects
       Queen testQueen = new Queen(1,1);
       Assert.Equal(false, testQueen.CanAttack(testQueen,1,1));
     }
+
+    [Fact]
+    public void Queen_ForUnalignedCoordinates_false()
+    {
+      Queen testQueen = new Queen(1,1);
+      Assert.Equal(false, testQueen.CanAttack(testQueen,3,2));
+    }
   }
 }
